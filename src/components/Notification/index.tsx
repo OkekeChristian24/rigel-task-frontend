181841e9436e89cf1df57fde35ce6c1ae90317d4
OkekeChristian24 <christian.okeke24@gmail.com>
2022-02-21 07:35:12 -0800

    chores: styled notification panel for the connected address

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,11 +1,24 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../state/reducers";
+import { StateType } from "../../state/reducers/reducers";
+import formatWallet from "../../helpers/formatWallet";
+import '../../style/components/notification.css';
 
 export default function Notification(){
 
+  const state: StateType = useSelector((state: RootState) => state.allData);
+
+
     return(
-        <div style={{textAlign: 'center', marginLeft: '20px'}}>
-  
-          
-  
+        <div className="notification-container">
+          {
+            state.address !== ""
+            &&
+            <p>
+              {(formatWallet(state.address))}
+
+            </p>
+          }
   
         </div>
     );
